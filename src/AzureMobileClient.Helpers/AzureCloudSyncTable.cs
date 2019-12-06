82865c7bdc0c6a305c9fc49bb2c96685712f188b
Joseph Hargreaves <Joseph.Hargreaves@EMEA.FS.UTC.com>
2019-12-06 16:40:49 +0000

    Fixed an issue with conflict handling. It was not taking the local version due to the faulty Equals implementation which only looked at the Id of the record, not comparing versions properly etc.

--- src/AzureMobileClient.Helpers/AzureCloudSyncTable.cs
+++ src/AzureMobileClient.Helpers/AzureCloudSyncTable.cs
@@ -166,14 +166,14 @@ namespace AzureMobileClient.Helpers
             var serverItem = error.Result.ToObject<T>();
             var localItem = error.Item.ToObject<T>();
 
-            // Note that you need to implement the public override Equals(TodoItem item)
-            // method in the Model for this to work
-            if (serverItem.Equals(localItem))
-            {
-                // Items are the same, so ignore the conflict
-                await error.CancelAndDiscardItemAsync();
-                return;
-            }
+            //// Note that you need to implement the public override Equals(TodoItem item)
+            //// method in the Model for this to work
+            //if (serverItem.Equals(localItem))
+            //{
+            //    // Items are the same, so ignore the conflict
+            //    await error.CancelAndDiscardItemAsync();
+            //    return;
+            //}
 
             // Client Always Wins
             localItem.Version = serverItem.Version;
